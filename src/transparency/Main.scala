a79ee9f45ab984150da71e66575c1791a636bd07
Panu Leppäniemi <me@panuleppaniemi.com>
2012-05-26 21:08:44 +0300

    Initial SimpleParser that creates Array of parsed Entries.

--- src/transparency/Main.scala
+++ src/transparency/Main.scala
@@ -1,21 +1,17 @@
 package transparency
 
-import entity.{Discussion}
+import entity._
 import parser.Parser
 
 object Main {
 
-  def main(args: Array[String]): Unit = {
+  def main(args: Array[String]): Unit = {    
+    val document = """|26.05.2012 14:00~Kent Beck~group0~TDD~What to test?
+                      |26.05.2012 14:01~Martin Fowler~group1~TDD~Everything
+                      |26.05.2012 14:03~Bob Martin~group2~TDD~Not everything""".stripMargin
     
-    val document = """|date0:name0:group0:topic0:message0
-                      |date1:name1:group1:topic1:message1
-                      |date2:name2:group2:topic2:message2""".stripMargin
-    
-    val content = (Parser createSpreadsheetParser) parse document
-        
-    println(content.length)
-    
-    var discussion = new Discussion("asd")
+    val content = (Parser simple) parse document
+    content.map(println)
   }
 
 }
--- src/transparency/parser/Parser.scala
+++ src/transparency/parser/Parser.scala
@@ -1,25 +1,36 @@
 package transparency.parser
 
-class Parser(mapping: Map[String, Int], 
-             lineSeparator: String, 
-             colSeparator: String) {
+class Parser(mapping: Map[String, Int], lineSep: String, colSep: String) {
   
-  def parse(document: String): Array[Array[String]] = {
-    (document split lineSeparator).map(_ split colSeparator)
+  def parse(document: String): Array[Parser.Entry] =
+    (document split lineSep) map(extract)
+  
+  private def extract(entry: String): Parser.Entry = {
+    val values = (entry split colSep)
+    
+    new Parser.Entry(
+      values(mapping("date")),
+      values(mapping("name")),
+      values(mapping("topic")),
+      values(mapping("message"))
+    )
   }
 }
 
 object Parser {
-  def createSimpleParser: Parser = {
+  def simple: Parser = {
     val mapping = 
       Map(
         "date"    -> 0,
         "name"    -> 1,
-        "group"   -> 2,
         "topic"   -> 3,
         "message" -> 4
       )
     
-    new Parser(mapping, "\n", ":")
+    new Parser(mapping, "\n", "~")
+  }
+  
+  class Entry(date: String, name: String, topic: String, message: String) {
+    override def toString = "[" + date + "] " + name + ": " + message
   }
 }
