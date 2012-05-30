eeeb0bdbcae05fe43f80431b9e107f6346b554c4
Panu Leppäniemi <me@panuleppaniemi.com>
2012-05-30 15:06:34 +0300

    Remove normal build files (and sources).

--- src/transparency/entity/Discussion.scala
+++
@@ -1,9 +0,0 @@
-package transparency.entity
-
-class Discussion(topic: String) {
-
-}
-
-// date:name:group:topic:message
-// date:name:group:topic:message
-// date:name:group:topic:message
--- src/transparency/entity/Message.scala
+++
@@ -1,7 +0,0 @@
-package transparency.entity
-
-import java.util.Date
-
-class Message(date: Date, message: String) {
-
-}
