229d494eeb5c72e4f6fc1c6506099ca177577eb9
Panu Leppäniemi <me@panuleppaniemi.com>
2012-10-22 16:41:00 +0300

    Fixed tests

--- src/test/scala/fi/metropolia/discussione/analyze/AnalyzerTest.scala
+++ src/test/scala/fi/metropolia/discussione/analyze/AnalyzerTest.scala
@@ -5,14 +5,14 @@ import org.scalatest._
 
 class AnalyzerTest extends FunSuite with BeforeAndAfter {
   val target: Analyzer = new Analyzer
-  val entry:Entry = new Entry("Anni", "2012-10-10", "puhe")
+  val unprocessed:Unprocessed = new Unprocessed("Anni", "2012-10-10", "puhe")
   
-  test("analyzeOne converts Entry to Processed") {    
-    assert(target.analyzeOne(entry).isInstanceOf[Processed])
+  test("analyzeOne converts Unprocessed to Processed") {    
+    assert(target.analyzeOne(unprocessed).isInstanceOf[Processed])
   }
   
   test("analyzeOne keyphrases message to Map with String key") {    
-    assert(target.analyzeOne(entry).keyphrases == Map("puhe" -> 1.0))
+    assert(target.analyzeOne(unprocessed).keyphrases == Map("puhe" -> 1.0))
   }
   
 }
